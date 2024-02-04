8a44a88952ff1dc9107ebcf5e922116ff1433b51
VYR4L <fkz.zanatt@gmail.com>
2024-02-04 00:08:08 -0300

    Header created

--- contact/urls.py
+++ contact/urls.py
@@ -4,8 +4,12 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 
+app_name = 'contact'
+
 urlpatterns = [
+     path('<int:contact_id>/', views.contact, name='contact'),
     path('', views.index, name='index'),
+   
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
